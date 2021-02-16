07555b3ec47811848b256003e4471af57a3eb0ce
Daddy Awesome <sablay296@gmail.com>
2021-02-16 20:38:32 +0000

    forms and views

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,20 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title       = forms.CharField(label='', widget = forms.TextInput(attrs={"placeholder":"Your Title"}))
+    description = forms.CharField(
+                        required = False,
+                        widget=forms.Textarea(
+                            attrs={
+                                "placeholder":"Your Description",
+                                "class":"new-class-name two",
+                                "id" : "my-id-for-text-area",
+                                "rows":20,
+                                "cola":120
+                            }
+                            ))
+    price       = forms.DecimalField(initial = 1.99)
+
     class Meta:
         model = Product
         fields = [
@@ -10,3 +24,18 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+class RawProductForm(forms.Form):
+    title       = forms.CharField(label='', widget = forms.TextInput(attrs={"placeholder":"Your Title"}))
+    description = forms.CharField(
+                        required = False,
+                        widget=forms.Textarea(
+                            attrs={
+                                "placeholder":"Your Description",
+                                "class":"new-class-name two",
+                                "id" : "my-id-for-text-area",
+                                "rows":20,
+                                "cola":120
+                            }
+                            ))
+    price       = forms.DecimalField(initial = 1.99)
